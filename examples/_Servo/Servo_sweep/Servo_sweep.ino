100e7c6170d4e834213c43c02afa08818e4fcb81
Andrey Kim <engineering.volosati@gmail.com>
2023-03-08 22:21:35 +0800

    Servo_sweep.ino 10:24 'A2 => 2'

--- examples/_Servo/Servo_sweep/Servo_sweep.ino
+++ examples/_Servo/Servo_sweep/Servo_sweep.ino
@@ -7,33 +7,39 @@
 */
 
 // The button must be connected between pin and Gnd
-const int buttonPin1 = A2;  //button1 Pin 2
-const int servoPin   = 3 ;  // The servo is connected to this pin
+const int buttonPin1 = 2; // button1 Pin 2
+const int servoPin = 3;   // The servo is connected to this pin
 
 // Position to sweep
 const byte target1 = 30;
 const byte target2 = 120;
 
 bool buttonPressed;
-byte targetPos = target1;    // actual targetposition for the servo
+byte targetPos = target1; // actual targetposition for the servo
 
 MoToServo myServo;
 
-void setup() {
+void setup()
+{
   pinMode(buttonPin1, INPUT_PULLUP);
 
   myServo.attach(servoPin);
-  myServo.setSpeed( 5 );    // set speed of servo
+  myServo.setSpeed(5); // set speed of servo
 }
 
-void loop() {
-  buttonPressed =  !digitalRead(buttonPin1); // 'ButtonPressed is 'true' if pin is LOW
+void loop()
+{
+  buttonPressed = !digitalRead(buttonPin1); // 'ButtonPressed is 'true' if pin is LOW
 
-  if (buttonPressed && not myServo.moving() ) {
-    if ( myServo.read() == target1 ) targetPos = target2;
-    if ( myServo.read() == target2 ) targetPos = target1;
-    myServo.write(targetPos); //will move slowly
+  if (buttonPressed && not myServo.moving())
+  {
+    if (myServo.read() == target1)
+      targetPos = target2;
+    if (myServo.read() == target2)
+      targetPos = target1;
+    myServo.write(targetPos); // will move slowly
   }
 
-  if ( !buttonPressed ) myServo.write( myServo.read() );    // stop immediately
+  if (!buttonPressed)
+    myServo.write(myServo.read()); // stop immediately
 }
